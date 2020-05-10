7c92b106ddd42c0cd3959766e54a9bc26aca82da
JuhyunLim <hcijh05@nate.com>
2020-05-11 00:06:09 +0900

    test-ing

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-var a = 1;
-var b = 2;
-var c = a + b;
-console.log(c);
-
-const a = 'Hello World!';
-console.log(a);
 const http = require('http'); // node 내장 모듈 불러옴
 const hostname = '127.0.0.1'; // localhost와 동일
 const port = 3000;
@@ -15,3 +8,5 @@ http
   })
   .listen(port, hostname);
 console.log('Server running at http://' + hostname + ':' + port);
+
+console.log('test-ing');
